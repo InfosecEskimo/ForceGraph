e2a46013c771031b1325cf630fb7e592118268ac
steven e <stevenyesc@gmail.com>
2020-09-09 18:06:01 -0400

    added visual node and group

--- src/Container/ForceGraph/ForceGraph.js
+++ src/Container/ForceGraph/ForceGraph.js
@@ -1,23 +1,43 @@
 import React from "react";
 import { ForceGraph2D } from "react-force-graph";
 
-const ForceGraph = ({ data }) => {
-  return (
-    <div>
-      <ForceGraph2D
-        onNodeHover={(node) => {
-          console.clear();
-          if (node === null) {
-            return;
-          }
-          console.log(`Node ID: ${node.id}`, `Node Group: ${node.group}`);
-        }}
-        enableNodeDrag={false}
-        backgroundColor="dark"
-        graphData={data}
-      />
-    </div>
-  );
-};
+class ForceGraph extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      nodeId: "",
+      nodeGroup: "",
+    };
+
+    this.setActiveNode = this.setActiveNode.bind(this);
+  }
+
+  setActiveNode(nodeId, nodeGroup) {
+    this.setState({ nodeId, nodeGroup });
+  }
+  render() {
+    return (
+      <div>
+        <ForceGraph2D
+          onNodeHover={(node) => {
+            if (node === null) {
+              this.setActiveNode("", "");
+              return;
+            }
+            this.setActiveNode(node.id, node.group);
+          }}
+          enableNodeDrag={false}
+          backgroundColor="dark"
+          graphData={this.props.data}
+        />
+        <div className="hover-block">
+          <p>NodeID: {this.state.nodeId}</p>
+          <p>NodeGroup: {this.state.nodeGroup}</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default ForceGraph;
